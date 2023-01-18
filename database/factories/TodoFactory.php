dd2b69620185d161827a9b77c50bbf808b53e7e5
Süleyman Özgür Özarpacı <oozarpac@gmail.com>
2023-01-18 18:45:32 +0300

    todo factory created

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
+ */
+class TodoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => \App\Models\User::factory(),
+            'title' => fake()->sentence(),
+            'note' => fake()->paragraph(),
+            'is_completed' => fake()->boolean(),
+            'completed_at' => fake()->dateTime(),
+        ];
+    }
+}
