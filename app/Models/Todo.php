ea2b9306a88edf04bef7400a96004b04571ee7e1
Süleyman Özgür Özarpacı <oozarpac@gmail.com>
2023-01-18 18:45:25 +0300

    todo model created

---
+++ app/Models/Todo.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Todo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'note',
+        'is_completed',
+        'completed_at',
+    ];
+
+    protected $casts = [
+        'is_completed' => 'boolean',
+        'completed_at' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
